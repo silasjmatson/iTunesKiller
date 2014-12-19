568c5eded1409006a3be0f92f7fd37e5a8485fab
Silas J. Matson <silas@clearsightstudio.com>
2014-12-18 17:31:18 -0800

    Initial Commit

---
+++ iTunesKiller/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  iTunesKiller
+//
+//  Created by Silas J. Matson on 12/18/14.
+//  Copyright (c) 2014 Silas J. Matson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iTunesKillerTests/iTunesKillerTests.swift
@@ -0,0 +1,36 @@
+//
+//  iTunesKillerTests.swift
+//  iTunesKillerTests
+//
+//  Created by Silas J. Matson on 12/18/14.
+//  Copyright (c) 2014 Silas J. Matson. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class iTunesKillerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
