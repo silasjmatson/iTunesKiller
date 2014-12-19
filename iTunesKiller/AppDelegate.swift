1db9a552ddd0b3517c8c5b0b45558a3e8b2055c5
Silas J. Matson <silas@clearsightstudio.com>
2014-12-18 18:56:47 -0800

    Initial App functional.

--- iTunesKiller/AppDelegate.swift
+++ iTunesKiller/AppDelegate.swift
@@ -3,8 +3,7 @@
 //  iTunesKiller
 //
 //  Created by Silas J. Matson on 12/18/14.
-//  Copyright (c) 2014 Silas J. Matson. All rights reserved.
-//
+//  
 
 import Cocoa
 
@@ -15,11 +14,38 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        NSWorkspace.sharedWorkspace().notificationCenter.addObserver(self, selector: "appLaunchedInSharedWorkspace:", name: NSWorkspaceDidLaunchApplicationNotification, object: nil);
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+        NSWorkspace.sharedWorkspace().notificationCenter.removeObserver(self);
+    }
+    
+    func appLaunchedInSharedWorkspace(notif: NSNotification!) {
+        var appName = notif.userInfo!["NSApplicationName"] as String;
+
+        if (appName == "iTunes") {
+            // Kill iTunes
+            var iTunesInstance = notif.userInfo!["NSWorkspaceApplicationKey"] as NSRunningApplication;
+            
+            killProgram(iTunesInstance);
+        }
+        // We don't care
+    }
+    
+    func killProgram(iTunesInstance: NSRunningApplication) {
+        if (iTunesInstance.terminate()) {
+            NSLog("Success! iTunes tried to be annoying and failed!");
+        } else {
+            NSLog("iTunes doesn't want to be killed. Let's nuke it!");
+            
+            if (iTunesInstance.forceTerminate()) {
+                NSLog("Finally. Killed the sucker.");
+            } else {
+                NSLog("This iTunes crap is a zombie. Maybe try a `kill -9`?");
+            }
+        }
+
     }
 
 
